c46585454d9db09cd6e4d88b30dca821788b4baf
alexisdiaz <lexmasta@gmail.com>
2020-04-10 15:33:53 -0400

    subprocess running shell command to wake screen added

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import subprocess
 
 class ScreenWakeOnWakeWord(MycroftSkill):
 		def __init__(self):
@@ -9,7 +9,8 @@ class ScreenWakeOnWakeWord(MycroftSkill):
 				self.add_event('recognizer_loop:wakeword', self.handle_wakeword)
 
 		def handle_wakeword(self, message):
-				self.speak("yes?")
+				# self.speak("yes?")
+				subprocess.run("xset dpms force on", shell=True)
 
 def create_skill():
     return ScreenWakeOnWakeWord()
